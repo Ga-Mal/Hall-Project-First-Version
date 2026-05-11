b49ae89193c442205f75815c7ad6a8756fca6775
Gamal Abdel fattah <gemy261@outlook.com>
2026-05-11 04:29:25 +0300

    EditButton

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router";
 export default function Button({ text, url }) {
   return (
     <div>
-      <Link to={url} className="mx-auto! py-3! block custom-button">
+      <Link to={url} className="mx-auto! text-lg! block custom-button">
         {text}
       </Link>
     </div>
